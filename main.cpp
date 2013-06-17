c522d35c650cac2bcc951afb7e84937e7d767799
Progerspro <edik-ruk@live.com>
2013-06-17 17:53:27 +0400

    Исправил пару недочётов
    Добавил структуру которой передаёться значения с функции Main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
-
-void output(string ur_name)
+struct person
 {
-  cout << "Welcome " << ur_name << endl;
+	char* name;
+	int age;
+	float weight;
+};
+
+void output(person p1)
+{
+  cout << "Welcome " << p1.name <<"   "<< p1.age <<"   "<< p1.weight  << endl;
 }
 
 int main()
 {
-  output("Edward");
+	person p_to = {"Edward",16,64.9};
+	output(p_to);
+  cin.get();
   return 0;
 }
